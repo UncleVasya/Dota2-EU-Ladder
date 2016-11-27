fee3bc21a121c92f8b1bce7395c72f89beb6b7bf
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-11-27 08:22:22 -0800

    #3 added OpenShift default wsgi settings

--- Dota2_EU_Ladder/wsgi.py
+++ Dota2_EU_Ladder/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dota2_eu_ladder.settings")
 
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
 application = get_wsgi_application()
