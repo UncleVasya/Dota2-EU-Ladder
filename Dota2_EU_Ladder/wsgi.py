7da0ce984a95b366f15e85a45f42729d217dfdec
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-11-27 08:35:22 -0800

    #3 adding project dir to sys.path

--- Dota2_EU_Ladder/wsgi.py
+++ Dota2_EU_Ladder/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
+import sys
 import os
+from os.path import dirname, realpath
 
 from django.core.wsgi import get_wsgi_application
 
+
+sys.path.append(dirname(realpath(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dota2_eu_ladder.settings")
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
