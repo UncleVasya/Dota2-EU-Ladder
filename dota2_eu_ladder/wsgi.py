dd1d1d32cd0639aa3bbd6842ff176054293ad9bb
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-11-28 10:25:50 +0300

    #3 fixed setup on non-openshift machines

--- dota2_eu_ladder/wsgi.py
+++ dota2_eu_ladder/wsgi.py
@@ -16,7 +16,7 @@ from django.core.wsgi import get_wsgi_application
 sys.path.append(dirname(realpath(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dota2_eu_ladder.settings")
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtenv = os.environ.get('OPENSHIFT_PYTHON_DIR', '.') + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
