0ad720ce27b5e00e9c05e3e6a5e6640948beac66
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-11-28 22:40:04 +0300

    #4 added django-autocomplete-light

--- dota2_eu_ladder/settings.py
+++ dota2_eu_ladder/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django-autocomplete-light
+    'dal',
+    'dal_select2',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
