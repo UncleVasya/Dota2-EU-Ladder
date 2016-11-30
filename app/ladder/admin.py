d473e9fca4efccde2a412e1a574856663ade9526
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-11-30 16:39:23 +0300

    #5 added Match model

--- app/ladder/admin.py
+++ app/ladder/admin.py
@@ -1,5 +1,6 @@
-from app.ladder.models import Player
+from app.ladder.models import Player, Match, MatchPlayer
 from django.contrib import admin
+from django.db.models import Prefetch
 
 
 class PlayerAdmin(admin.ModelAdmin):
@@ -13,4 +14,28 @@ class PlayerAdmin(admin.ModelAdmin):
     list_display = ('name', 'rank', 'score', 'mmr')
 
 
+class PlayerInline(admin.TabularInline):
+    model = MatchPlayer
+    min_num = 10
+    max_num = 10
+
+
+class MatchAdmin(admin.ModelAdmin):
+    model = Match
+
+    fieldsets = [
+        (None, {'fields': ['date', 'winner']}),
+    ]
+    readonly_fields = ['date']
+
+    inlines = (PlayerInline, )
+
+    list_display = ('date', )
+
+    def get_queryset(self, request):  # performance optimisation
+        qs = super(MatchAdmin, self).get_queryset(request)
+        return qs.prefetch_related(Prefetch('players'))
+
+
 admin.site.register(Player, PlayerAdmin)
+admin.site.register(Match, MatchAdmin)
