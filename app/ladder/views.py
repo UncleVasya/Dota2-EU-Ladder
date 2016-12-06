8821f29b1db49c6f33a307e5a8fc0ca27604fd5e
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-12-06 14:14:23 +0300

    #8 fixed repetitive names on leaderboard

--- app/ladder/views.py
+++ app/ladder/views.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView, DetailView
 
 class PlayerList(ListView):
     # those who played at least 1 game
-    queryset = Player.objects.filter(matchplayer__isnull=False)
+    queryset = Player.objects.filter(matchplayer__isnull=False).distinct()
 
     def get_context_data(self, **kwargs):
         context = super(PlayerList, self).get_context_data(**kwargs)
