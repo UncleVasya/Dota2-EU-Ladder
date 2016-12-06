b7a52969905cb966589b2c83cf073974ca9dfab1
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-12-06 13:12:33 +0300

    #8 rank people using ladder-mmr

--- dota2_eu_ladder/managers.py
+++ dota2_eu_ladder/managers.py
@@ -21,13 +21,13 @@ class PlayerManager(models.Manager):
 
     def update_ranks(self):
         # recalculate player rankings based on score
-        score_groups = defaultdict(list)
+        mmr_groups = defaultdict(list)
         for player in self.all():
-            score_groups[player.score].append(player)
+            mmr_groups[player.ladder_mmr].append(player)
 
-        score_groups = sorted(score_groups.items(), reverse=True)
+        mmr_groups = sorted(mmr_groups.items(), reverse=True)
 
-        for rank, group in enumerate(score_groups):
+        for rank, group in enumerate(mmr_groups):
             for player in group[1]:
                 player.rank = rank + 1
                 player.save()
