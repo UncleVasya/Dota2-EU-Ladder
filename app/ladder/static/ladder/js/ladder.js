e3577a2108d3069dea3fac0005733fe71a24c7eb
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-12-08 15:14:21 +0300

    #7 leaderboard is now sortable

--- app/ladder/static/ladder/js/ladder.js
+++ app/ladder/static/ladder/js/ladder.js
@@ -1,6 +1,11 @@
 var main = function() {
     // transform time values into 'n days ago'
     $('.timeago').timeago();
+
+    $('table.tablesorter').tablesorter({
+        sortList: [[2,1]],  // 3rd column sorted desc
+        sortStable: true
+    });
 };
 
 $(document).ready(main);
