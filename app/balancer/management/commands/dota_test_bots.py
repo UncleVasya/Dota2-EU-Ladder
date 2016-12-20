e3f7938a6dc603c7223fe416272da0d6da895a76
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-12-20 12:44:43 +0300

    #10 test bots join right slots after the balance

--- app/balancer/management/commands/dota_test_bots.py
+++ app/balancer/management/commands/dota_test_bots.py
@@ -4,6 +4,7 @@ import dota2
 import os
 from steam import SteamClient
 from dota2 import Dota2Client
+from dota2.enums import DOTAChatChannelType_t, DOTA_GC_TEAM
 
 
 class Command(BaseCommand):
@@ -11,6 +12,8 @@ class Command(BaseCommand):
         self.bots = []
         self.lobby = None
         self.password = None
+
+        self.balance_team1 = None
 
     def add_arguments(self, parser):
         parser.add_argument('-l', '--lobby', type=int)
@@ -70,9 +73,58 @@ class Command(BaseCommand):
             print '%s joined lobby %s' % (dota.steam.username, lobby.lobby_id)
 
             ind = self.bots.index(dota)
+
+            if ind == 0:
+                # let first bot listen to lobby chat
+                dota.join_lobby_chat()
+
             team = ind / 5
             slot = ind % 5 + 1
             dota.join_practice_lobby_team(slot, team)
 
+        @dota.on(dota2.features.Lobby.EVENT_LOBBY_CHANGED)
+        def lobby_changed(lobby):
+            if dota != self.bots[0]:
+                return
+
+            players = [
+                player for player in lobby.members
+                if player.team in (DOTA_GC_TEAM.GOOD_GUYS, DOTA_GC_TEAM.BAD_GUYS)
+            ]
+
+            if len(players) == 0 and self.balance_team1:
+                print
+                print 'All bots jumped to unassigned'
+                self.join_balance_slots()
+
+        @dota.on(dota2.features.Chat.EVENT_CHAT_MESSAGE)
+        def chat_message(channel, sender, text, msg_obj):
+            if channel.channel_type != DOTAChatChannelType_t.DOTAChannelType_Lobby:
+                return  # ignore postgame and other chats
+
+            # process known commands
+            if text.startswith('Team 1'):
+                self.balance_ready(text)
+
         client.login(credentials['login'], credentials['password'])
         client.run_forever()
+
+    def balance_ready(self, team1_text):
+        print
+        print 'Balance is ready'
+
+        self.balance_team1 = team1_text.split(': ')[1].split(' | ')
+
+        print 'Telling bots to free slots'
+        for bot in self.bots:
+            bot.join_practice_lobby_team()
+
+    def join_balance_slots(self):
+        print
+        print 'Joining balanced slots'
+
+        players_joined = [0, 0]
+        for bot in self.bots:
+            team = 0 if bot.steam.username in self.balance_team1 else 1
+            players_joined[team] += 1
+            bot.join_practice_lobby_team(players_joined[team], team)
