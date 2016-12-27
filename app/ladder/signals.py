df83ed63622b52df2b0a205176b7a29c4c41cfc1
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-12-27 18:43:16 +0300

    #10 code cleanup in signals

--- app/ladder/signals.py
+++ app/ladder/signals.py
@@ -6,31 +6,18 @@ from app.ladder.models import ScoreChange, Match, Player
 
 
 @receiver([post_save, post_delete], sender=ScoreChange)
-def score_change(sender, instance, **kwargs):
-    print '\n'
-    print 'score_change signal'
-    print 'sender: %s  instance: %s  kwargs: %s' % (sender, instance, kwargs)
-
+def score_change(instance, **kwargs):
     player = instance.player
 
-    player.score = player.scorechange_set.aggregate(
-        Sum('score_change')
-    )['score_change__sum']
+    vals = player.scorechange_set.aggregate(
+        Sum('mmr_change'), Sum('score_change'))
 
-    player.ladder_mmr = player.scorechange_set.aggregate(
-        Sum('mmr_change')
-        )['mmr_change__sum']
+    player.ladder_mmr = vals['mmr_change__sum']
+    player.score = vals['score_change__sum']
 
     player.save()
 
-    print 'Player mmr, score: %s, %s' % (player.ladder_mmr, player.score)
-
 
 @receiver(post_delete, sender=Match)
-def match_change(sender, instance, **kwargs):
-    print '\n'
-    print 'match_change signal'
-    print 'sender: %s  instance: %s  kwargs: %s' % (sender, instance, kwargs)
-
-    print 'Updating ranks'
+def match_change(**kwargs):
     Player.objects.update_ranks()
