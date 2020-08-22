ca5de557dd1abb4db616de11c3d37018d1d05a6e
UncleVasya <unclevasyaai@gmail.com>
2020-08-22 11:27:01 +0300

    Revert "#50: Don't need wsgi.py after OpenShift v2"
    I need it to use runserver locally
    
    This reverts commit e7393e8e9b3765b440fe7e7a5aca108c50f801d7.

---
+++ dota2_eu_ladder/wsgi.py
@@ -0,0 +1,26 @@
+"""
+WSGI config for dota2_eu_ladder project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+import sys
+import os
+from os.path import dirname, realpath
+
+from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append(dirname(realpath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dota2_eu_ladder.settings")
+
+virtenv = os.environ.get('OPENSHIFT_PYTHON_DIR', '.') + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+application = get_wsgi_application()
